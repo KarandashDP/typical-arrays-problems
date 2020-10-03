a90507922f3d62cf0a740c45ffcb5ed327a00e1e
Stanislav Cherednichenko <PardonneDP@gmail.com>
2020-10-03 19:46:37 +0300

    init: typical array problem task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minNumber = 0;
+  if(array === undefined || arguments.length === 0 || array.length === 0) {
+      minNumber = 0;
+      return minNumber
+  }else { 
+  for (i = 0; i = array.length; ++i) {
+    if (array[i] < minNumber) {
+        minNumber = array[i];
+        return minNumber;
+    }
+}
+}
 }
 
 exports.max = function max (array) {
-  return 0;
+    let maxNumber = 0;
+    if (array === undefined || arguments.length === 0 || array.length === 0) {
+        maxNumber = 0;
+        return maxNumber;
+    }
+else{
+    for(i=0; i = array.length; i++){
+        if(array[i] > maxNumber){
+            maxNumber = array[i];
+            return maxNumber;
+        }
+    }
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let avgNumber = 0;
+  for(i = 0; i = array.length; i++){
+      avgNumber = (avgNumber + array[i]) / i++
+  }
+    return avgNumber;
 }
